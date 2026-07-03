40d28e21816c7ffce39d064af68c3abb7fed79f1
themorli <108215801+themorli@users.noreply.github.com>
2026-07-03 19:36:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => {
-    card.addEventListener('mousemove', e => {
-        const rect = card.getBoundingClientRect();
-
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        card.style.setProperty('--x', `${x}px`);
-        card.style.setProperty('--y', `${y}px`);
-    });
-});
+console.log('Speaker website loaded');
